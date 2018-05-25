dea4b89d2532cb5bfed9223d5178b9d58a2390dc
ayan-b <ayanbanerjee7777@gmail.com>
2018-05-25 16:58:29 +0530

    Revamp UI in blog section

--- personal/views.py
+++ personal/views.py
@@ -4,6 +4,4 @@ def index(request):
     return render(request, 'personal/home.html')
 
 def contact(request):
-    return render(request, 'personal/basic.html', {
-        'content':['If you would like to contact me, please email me ayanbn7@gmail.com or call me at 1234567890'],
-        })
+    return render(request, 'personal/basic.html',)
